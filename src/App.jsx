0a798e066d7edba0532d47052281caf3ed1d5355
Jose Corredor <jose96corrza@gmail.com>
2023-02-26 23:06:52 -0500

    initialLoading page set

--- src/App.jsx
+++ src/App.jsx
@@ -9,40 +9,42 @@ import { LoadingPage } from "./pages/LoadingPage"
 
 function App() {
   // const [characters , setCharacters] = useState([])
-  // const [isLoading, setIsLoading] = useState(true)
+  const [initialLoading, setInitialLoading] = useState(true)
 
 
-  // useEffect(() => {
-  //     fetch('https://rickandmortyapi.com/api/character')
-  //     .then(response => response.json())
-  //     .then(data => {
-  //         setCharacters(data.results)
-  //         setTimeout(() => {
-  //             setIsLoading(!isLoading)
-  //           }, 1500)
-  //           // setIsLoading(!isLoading)
-  //         })
-  //       },[])
+  useEffect(() => {
+      setTimeout(() => {
+        setInitialLoading(false)
+      }, 2000);
+  },[])
+
         const [currentPage, setCurrentPage] = useState(2)
         let API = `https://rickandmortyapi.com/api/character?page=${currentPage}`
         const { characters, isLoading, setIsLoading } = useFetchApi(API, 3000, [currentPage])
   return (
     // <div className='dark:bg-slate-500 bg-gray-200 w-full h-full'> `url(${bg})`
     <>
-    <Header />
-    <div className='bg-fixed h-max' style={{backgroundImage: 'url(/background.jpeg)'}}>
-      
-        <>
-          {/* <Header /> */}
-          <Characters 
-            data={characters} 
-            currentPage={currentPage} 
-            setCurrentPage={setCurrentPage}
-            setIsLoading={setIsLoading}
-            isLoading={isLoading}
-          />
-        </>
-    </div>
+      {
+      initialLoading ?
+      <LoadingPage />
+      :
+      (<>
+        <Header />
+        <div className='bg-fixed h-max' style={{backgroundImage: 'url(/background.jpeg)'}}>
+          
+            <>
+              {/* <Header /> */}
+              <Characters 
+                data={characters} 
+                currentPage={currentPage} 
+                setCurrentPage={setCurrentPage}
+                setIsLoading={setIsLoading}
+                isLoading={isLoading}
+              />
+            </>
+        </div>
+        </>)
+      }
     </>
   )
 }
